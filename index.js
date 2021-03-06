94e0e745941c04936b66992628ad7af1b3fe8986
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-06 12:34:49 +0000

    add path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const data = require("./data/raw/json/routes-data-final.json");
 
 const app = express();
