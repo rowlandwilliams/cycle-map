5b9df2852c59ff47a568f93070b466b123e6d225
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-07 13:12:51 +0000

    add horizontal slide to arrow

--- client/src/components/Selector/Selector.js
+++ client/src/components/Selector/Selector.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
+import SelectorArrow from "./SelectorArrow";
 
 function Selector() {
+  const [boxVisible, setBoxVisible] = useState(true);
+
+  const onArrowClick = () => {
+    setBoxVisible(!boxVisible);
+  };
   return (
     <div className="sc-wrapper">
-      <div className="sc-item sc-item--box"></div>
-      <div className="sc-item sc-item--arrow">&#xab;</div>
+      <div className={`sc-item sc-item--box ${boxVisible ? "" : "hidden"}`}>
+        SUH
+        <div>SUDEDE</div>
+      </div>
+      <SelectorArrow onArrowClick={onArrowClick} />
     </div>
   );
 }
---
+++ client/src/components/Selector/SelectorArrow.js
@@ -0,0 +1,9 @@
+function SelectorArrow(props) {
+  return (
+    <div className="sc-item sc-item--arrow" onClick={props.onArrowClick}>
+      &#xab;
+    </div>
+  );
+}
+
+export default SelectorArrow;
