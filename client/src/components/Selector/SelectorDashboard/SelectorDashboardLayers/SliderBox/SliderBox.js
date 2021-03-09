2b7e9adb0db9fb05f7342eb06ceca61e2cff1eb4
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-09 13:20:56 +0000

    create slider box component (non-agnostic)

---
+++ client/src/components/Selector/SelectorDashboard/SelectorDashboardLayers/SliderBox/SliderBox.js
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeSlider } from "../../../../../actions/index";
+
+import "./styles.css";
+
+function SliderBox(props) {
+  const tripsSlider = useSelector((state) => state.tripsSlider);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="sc-db-slider">
+      <div>Tail-length: </div>
+      <div className="sc-db-slider sc-db-slider__row2">
+        <input
+          style={{ width: "50%" }}
+          type="range"
+          min={tripsSlider.min}
+          max={tripsSlider.max}
+          step={tripsSlider.step}
+          value={tripsSlider.value}
+          onChange={(e) => dispatch(changeSlider(e))}
+        />
+        <div
+          className="sc-db-slider sc-db-slider__row2 sc-db-slider__row2--value"
+          style={{ backgroundColor: props.bgColor }}
+        >
+          {tripsSlider.value}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SliderBox;
