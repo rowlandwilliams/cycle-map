ab4dfc020a9b7247bef1e942052282ed4bfc78c3
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-10 16:17:32 +0000

    add col scale to legend

--- client/src/components/SelectorRight/children/Legend/Legend.js
+++ client/src/components/SelectorRight/children/Legend/Legend.js
@@ -1,35 +1,31 @@
 import { useSelector } from "react-redux";
 import TripSvg from "./svg/TripSvg";
 
+const tripColours = require("../../../utils/tripColours.json")[0].hex;
+const labels = ["<0.5", ">1", ">2", ">3", ">4", ">5", ">10", ">20"];
+
 function Legend() {
   const tripsVisible = useSelector((state) => state.tripsVisible.tripsVisible);
   const stationsVisible = useSelector(
     (state) => state.stationsVisible.stationsVisible
   );
-  const colours = ["#fd805d", "#30c985"];
-  console.log(colours[1]);
 
-  console.log("suh", tripsVisible, stationsVisible);
   return (
     <div className="lg-container">
       <div className="lg-row">
         <div>Trip Length</div>
         <div>Km</div>
       </div>
-      <TripSvg
-        label="<5"
-        startColour="#ffffff"
-        endColour={colours[0]}
-        gradientId="5k"
-        svgClass="trip-svg"
-      />
-      <TripSvg
-        label="5+"
-        startColour="#ffffff"
-        endColour={colours[1]}
-        gradientId="10k"
-        svgClass="trip-svg"
-      />
+      {tripColours.map((col, i) => (
+        <TripSvg
+          key={labels[i]}
+          label={labels[i]}
+          startColour="#ffffff"
+          endColour={col}
+          gradientId={labels[i]}
+          svgClass="trip-svg"
+        />
+      ))}
     </div>
   );
 }
