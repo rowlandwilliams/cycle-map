e6886bc2d54748044a44ab0864602256ff89ff7c
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-10 18:04:28 +0000

    rm padding from right dropdown to hide legend when empty

--- client/src/components/SelectorRight/children/Legend/Legend.js
+++ client/src/components/SelectorRight/children/Legend/Legend.js
@@ -1,8 +1,6 @@
 import { useSelector } from "react-redux";
-import TripSvg from "./svg/TripSvg";
-
-const tripColours = require("../../../utils/tripColours.json")[0].hex;
-const labels = ["<0.5", ">1", ">2", ">3", ">4", ">5", ">10", ">20"];
+import TripLegend from "./TripLegend";
+import "./styles.css";
 
 function Legend() {
   const tripsVisible = useSelector((state) => state.tripsVisible.tripsVisible);
@@ -11,22 +9,7 @@ function Legend() {
   );
 
   return (
-    <div className="lg-container">
-      <div className="lg-row">
-        <div>Trip Length</div>
-        <div>Km</div>
-      </div>
-      {tripColours.map((col, i) => (
-        <TripSvg
-          key={labels[i]}
-          label={labels[i]}
-          startColour={col}
-          endColour={col}
-          gradientId={labels[i]}
-          svgClass="trip-svg"
-        />
-      ))}
-    </div>
+    <div className="lg-container">{tripsVisible ? <TripLegend /> : null}</div>
   );
 }
 
---
+++ client/src/components/SelectorRight/children/Legend/TripLegend.js
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import TripSvg from "./svg/TripSvg";
+
+const tripColours = require("../../../utils/tripColours.json")[0].hex;
+const labels = ["<0.5", ">1", ">2", ">3", ">4", ">5", ">10", ">20"];
+
+function TripLegend() {
+  return (
+    <Fragment>
+      <div className="lg-row">
+        <div>Trip Length</div>
+        <div>Km</div>
+      </div>
+      {tripColours.map((col, i) => (
+        <TripSvg
+          key={labels[i]}
+          label={labels[i]}
+          startColour={col}
+          endColour={col}
+          gradientId={labels[i]}
+          svgClass="trip-svg"
+        />
+      ))}
+    </Fragment>
+  );
+}
+
+export default TripLegend;
