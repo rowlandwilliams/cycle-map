3ca728de9e54fdbbf1af663c30460faaea7b3a14
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-03-12 18:06:47 +0000

    add trips from/to each station

--- data/processing-scripts/docking-stations-processing.js
+++ data/processing-scripts/docking-stations-processing.js
@@ -3,6 +3,13 @@ const csv = require("csv-parser");
 
 const stations = [];
 const trips = require("../raw/json/journey-data-matched.json");
+const tripsDistance = require("../raw/json/routes-data-final.json");
+trips.forEach(
+  (trip) =>
+    (trip.distance = tripsDistance.filter(
+      (x) => x.rental_id === trip.rental_id
+    )[0].distance)
+);
 
 fs.createReadStream("./data/raw/docking-stations.csv")
   .pipe(csv())
@@ -23,9 +30,26 @@ fs.createReadStream("./data/raw/docking-stations.csv")
         x.endstation_id == row.station_id || x.startstation_id == row.station_id
     ).length;
 
+    row.trips = trips
+      .filter(
+        (x) =>
+          x.endstation_id == row.station_id ||
+          x.startstation_id == row.station_id
+      )
+      .map((x) => ({
+        station:
+          x.startstation_id === row.station_id // if start station in each trip is same stations id then trip is outbound to grab the Destination
+            ? x.endstation_name
+            : x.startstation_name,
+        duration: x.duration,
+        type: x.startstation_id === row.station_id ? "outbound" : "inbound",
+        distance: x.distance,
+      }));
+
     stations.push(row);
   })
   .on("end", () => {
+    // console.log(stations);
     fs.writeFileSync(
       "./data/raw/json/docking-stations-processed.json",
       JSON.stringify(stations)
